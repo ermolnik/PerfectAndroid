a301a953ff21efd62309e75a721172b3350d3244
Сергей Ермолаев <ermolnik666@gmail.com>
2019-05-03 00:29:28 +0300

    has been added default database config with user entity

---
+++ data/src/main/java/com/ermolnik/perfectandroid/db/userinfo/UserEntity.java
@@ -0,0 +1,43 @@
+package com.ermolnik.perfectandroid.db.userinfo;
+
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class UserEntity {
+    @PrimaryKey
+    private long id;
+    private String firstName;
+    private String lastName;
+
+    public UserEntity(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
